5291fc5081085499df5416688d0b325c3958727a
BlockableDev <blockabledev@gmail.com>
2023-02-15 17:20:23 +0000

    Gastfamilie

--- www/login.js
+++ www/login.js
@@ -1,33 +1,30 @@
-const {WebUntis} = require("webuntis")
+const { WebUntis } = require("webuntis")
 var storage = window.localStorage
-var username = storage.getItem(uname)
+var username = storage.getItem("uname")
 
 function logger() {
-function loginData() {
-    uname = document.getElementById("uname").value;
-    psw = document.getElementById("psw").value;
-}
-loginData()
-function MegaWriter() {
-    storage.setItem(password, psw)
-    storage.setItem(username, uname)
-}
-if (username == null) {
-    loginData()
-    MegaWriter()
-    document.getElementById("teste").innerHTML(amen)
-}else {
-    document.getElementById("teste").innerHTML(username)
-}
-}
-
-var usernameWriter = storage.setItem(password, psw)
-var passwordWriter = storage.setItem(username, uname)
-
-const menu_toggle = document.querySelector(".menu-toggle");
-const sidebar = document.querySelector(".sidebar");
-
-menu_toggle.addEventListener("click", () => {
-    menu_toggle.classList.toggle("is-active");
-    sidebar.classList.toggle("is-active");
-});
+     console.log("teste")
+     function loginData() {
+            uname = document.getElementById("uname").value
+            psw = document.getElementById("psw").value
+            console.log(psw)
+            console.log(uname)
+        }
+        loginData()
+        function MegaWriter() {
+             storage.setItem("password", psw)
+             storage.setItem("username", uname)
+             ultra = storage.getItem("username")
+             console.log(ultra)
+            }
+            if (uname == null) {
+                loginData()
+                MegaWriter()
+                document.getElementById("teste").innerHTML = "amen"
+            } else {
+                document.getElementById("teste").innerHTML = uname
+                const untis = new WebUntis("dsporto", uname, psw, "borys.webuntis.com")}
+            }
+            //var usernameWriter = storage.setItem(password, psw)
+            //var passwordWriter = storage.setItem(username, uname)
+            
